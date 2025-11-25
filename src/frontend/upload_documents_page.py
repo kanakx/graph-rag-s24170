5e62d66013b8c0508c815dac164e3b2743a95bfd
kanakx <kacperkanak@icloud.com>
2025-11-25 15:20:59 +0100

    Implement dummy file uploader for document uploads

--- src/frontend/upload_documents_page.py
+++ src/frontend/upload_documents_page.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title("Upload Documents")
+uploaded_files = st.file_uploader(
+    "Upload Documents", accept_multiple_files=True, type="pdf"
+)
+for uploaded_file in uploaded_files:
+    pass  # TODO: File processing here
