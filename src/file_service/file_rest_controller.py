a6bae93225bb218feefaf71701c8e88f2c690d98
kanakx <kacperkanak@icloud.com>
2025-11-26 17:18:30 +0100

    Configure MinIO client and load environment variables for file uploads

--- src/file_service/file_rest_controller.py
+++ src/file_service/file_rest_controller.py
@@ -1,15 +1,29 @@
+import os
+
+from minio import Minio
+from dotenv import load_dotenv
+
 from fastapi import FastAPI, UploadFile, File
+
+load_dotenv('.env.dev')
 
 app = FastAPI()
 
+minio_client = Minio(
+    endpoint=os.getenv('MINIO_ENDPOINT', 'localhost:9000'),
+    access_key=os.getenv('MINIO_ACCESS_KEY', 'MINIO_ROOT_USER'),
+    secret_key=os.getenv('MINIO_SECRET_KEY', 'MINIO_ROOT_PASSWORD'),
+    secure=False,
+)
 
-@app.post("/upload")
+
+@app.post('/upload')
 async def upload_file(file: UploadFile = File(...)):
     content = await file.read()
     size = len(content)
 
     return {
-        "filename": file.filename,
-        "content_type": file.content_type,
-        "size": size,
+        'filename': file.filename,
+        'content_type': file.content_type,
+        'size': size,
     }
