b5688d4429554ffdf206e5d1ee3bff60fddfd3a6
kanakx <kacperkanak@icloud.com>
2025-11-27 19:50:33 +0100

    Refactor import statements in ingestion_rest_controller.py for improved readability

--- src/ai_service/app/ingestion_rest_controller.py
+++ src/ai_service/app/ingestion_rest_controller.py
@@ -3,9 +3,9 @@ import os
 from fastapi import FastAPI
 import requests
 
-from src.ai_service.app.file_info import FileInfo
-from src.ai_service.app.graph_transformer import ingest_cv_text_into_graph
-from src.ai_service.app.pdf_util import pdf_to_text
+from file_info import FileInfo
+from graph_transformer import ingest_cv_text_into_graph
+from pdf_util import pdf_to_text
 
 FILE_SERVICE_URL = os.getenv('FILE_SERVICE_URL')
 
